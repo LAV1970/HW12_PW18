3e76b92e1318ba840805fc59ff092bf5e4e1c70c
lomakina0612 <lomakina0612@gmail.com>
2024-02-22 09:29:27 +0100

    22_02_db_bild

--- db/__init__.py
+++ db/__init__.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://lomakin:QwertY_12345@localhost/test12"
@@ -7,4 +7,4 @@ SQLALCHEMY_DATABASE_URL = "postgresql://lomakin:QwertY_12345@localhost/test12"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base: DeclarativeMeta = declarative_base()
+Base = declarative_base()
