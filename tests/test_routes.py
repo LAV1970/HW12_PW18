1b37809ad75fd08ec54d73e82980d2552121188f
lomakina0612 <lomakina0612@gmail.com>
2024-03-09 17:28:12 +0100

    hw14_95_1

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -11,4 +11,9 @@ def client():
 def test_get_notes(client):
     response = client.get("/notes")
     assert response.status_code == 200
-    assert response.json() == {"message": "Get all notes"}
+    data = response.json()
+    assert "notes" in data  # Проверяем наличие ключа "notes" в JSON-ответе
+    assert isinstance(data["notes"], list)  # Проверяем, что "notes" - это список
+    assert all(
+        "id" in note and "title" in note for note in data["notes"]
+    )  # Проверяем, что каждая заметка в списке имеет "id" и "title"
