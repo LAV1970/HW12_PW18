f7b868b09d4a4381cf386a9bf8f03e62c778cadc
lomakina0612 <lomakina0612@gmail.com>
2024-03-09 13:54:44 +0100

    hw14_docs

--- source/conf.py
+++ source/conf.py
@@ -6,23 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'hw14PC18'
-copyright = '2024, Lomakin'
-author = 'Lomakin'
-release = '1.0'
+project = "hw14PC18"
+copyright = "2024, Lomakin"
+author = "Lomakin"
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+extensions = [
+    # ...
+    "sphinx.ext.autodoc",
+    # ...
+]
